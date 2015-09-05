697df2da6e5630a8afc402c4dad9b190feb046e4
Matías Fernández <recta.de.euler@gmail.com>
2015-09-05 16:03:34 -0300

    Cambio test y agrego dos test nuevos.

--- src/com/vectorns/TavernaClientExampleTest.java
+++ src/com/vectorns/TavernaClientExampleTest.java
@@ -1,8 +1,6 @@
 package com.vectorns;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -27,10 +25,10 @@ import org.junit.Test;
 
 public class TavernaClientExampleTest {
 
-	// TODO: private static final String TAVERNA_REST_API_URL =
-	// "http://localhost:8080/taverna-server/rest";
-	private static final String TAVERNA_REST_API_URL = "http://localhost:9000/";
-	private static final String WORKFLOW_TEST_FILE = "WorkflowTesting.t2flow";
+	private static final String WORKBERCH_REST_API_URL = "http://localhost:9000";
+	private static final String TAVERNA_REST_API_URL = "http://localhost:8080/TavernaServer.2.4.4/rest";
+	private static final String WORKFLOW_TEST_FILE = "WorkflowTestingSmall.t2flow";
+	private static final String INPUT_TEST_FILE = "input.xml";
 
 	private String getFileOnString(final String filePath) throws IOException {
 		final BufferedReader bufferReader = new BufferedReader(new FileReader(filePath));
@@ -49,8 +47,7 @@ public class TavernaClientExampleTest {
 		}
 	}
 
-	//TODO: Add this before to clean taverna server @Before
-	public void cleanServer() {
+	private void cleanServer() {
 		final CredentialsProvider credsProvider = new BasicCredentialsProvider();
 		credsProvider.setCredentials(new AuthScope("localhost", 8080), new UsernamePasswordCredentials("taverna", "taverna"));
 		final CloseableHttpClient httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
@@ -74,7 +71,65 @@ public class TavernaClientExampleTest {
 	}
 
 	@Test
-	public void testPostRuns() throws UnsupportedEncodingException, IOException, InterruptedException {
+	public void testWorkberchPostRuns() throws UnsupportedEncodingException, IOException, InterruptedException {
+		final CloseableHttpClient httpClient = HttpClients.custom().setMaxConnTotal(100).setMaxConnPerRoute(50).build();
+
+		// Step 1
+		StringEntity reqEntity = new StringEntity(getFileOnString(WORKFLOW_TEST_FILE));
+		reqEntity.setContentType("application/vnd.taverna.t2flow+xml");
+
+		final HttpPost httpPost = new HttpPost(WORKBERCH_REST_API_URL + "/runs");
+		httpPost.setEntity(reqEntity);
+		httpPost.setHeader("ContentType", "application/vnd.taverna.t2flow+xml");
+
+		CloseableHttpResponse response = httpClient.execute(httpPost);
+		assertEquals(201, response.getStatusLine().getStatusCode());
+		
+		final String[] urlParts = response.getHeaders("Location")[0].getValue().split("/");
+		final String workflowUUID = urlParts[urlParts.length - 1];
+
+		// Step 2
+		final File inputFile = new File(INPUT_TEST_FILE);
+		assertTrue(inputFile.exists());
+
+		reqEntity = new StringEntity(getFileOnString(INPUT_TEST_FILE));
+		reqEntity.setContentType("application/xml");
+
+		HttpPut httpPutValues = new HttpPut(WORKBERCH_REST_API_URL + "/runs/" + workflowUUID + "/input/input/input");
+		httpPutValues.setEntity(reqEntity);
+		httpPutValues.setHeader("ContentType", "application/xml");
+
+		response = httpClient.execute(httpPutValues);
+		assertEquals(200, response.getStatusLine().getStatusCode());
+		
+		// Step 3
+		final HttpPut httpPutRun = new HttpPut(WORKBERCH_REST_API_URL + "/runs/" + workflowUUID + "/status");
+		httpPutRun.setEntity(new StringEntity("Operating"));
+		httpPutRun.setHeader("ContentType", "text/plain");
+
+		response = httpClient.execute(httpPutRun);
+		assertEquals(200, response.getStatusLine().getStatusCode());
+
+		// Step 4
+		final HttpGet httpGetStatus = new HttpGet(WORKBERCH_REST_API_URL + "/runs/" + workflowUUID + "/status");
+		String workflowStatus;
+		do {
+			workflowStatus = httpClient.execute(httpGetStatus, new BasicResponseHandler());
+			Thread.sleep(1000);
+		} while (workflowStatus.equalsIgnoreCase("Operating"));
+
+		// Step 5
+		final HttpGet httpGetOutputDescription = new HttpGet(WORKBERCH_REST_API_URL + "/runs/" + workflowUUID + "/wd/out");
+		httpGetOutputDescription.setHeader("Accept", "application/xml");
+		final String outputDirectoryParts = httpClient.execute(httpGetOutputDescription, new BasicResponseHandler());
+		assertTrue(!outputDirectoryParts.isEmpty());
+	}
+
+	
+	@Test
+	public void testTavernaPostRuns() throws UnsupportedEncodingException, IOException, InterruptedException {
+		cleanServer();
+		
 		final CredentialsProvider credsProvider = new BasicCredentialsProvider();
 		credsProvider.setCredentials(new AuthScope("localhost", 8080), new UsernamePasswordCredentials("taverna", "taverna"));
 		final CloseableHttpClient httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).setMaxConnTotal(100)
@@ -90,24 +145,24 @@ public class TavernaClientExampleTest {
 
 		CloseableHttpResponse response = httpClient.execute(httpPost);
 		assertEquals(201, response.getStatusLine().getStatusCode());
-
-		// Step 2
-		final File inputFile = new File(WORKFLOW_TEST_FILE);
-		assertTrue(inputFile.exists());
-
-		reqEntity = new StringEntity(getFileOnString("input.xml"));
-		reqEntity.setContentType("application/xml");
-
+		
 		final String[] urlParts = response.getHeaders("Location")[0].getValue().split("/");
 		final String workflowUUID = urlParts[urlParts.length - 1];
 
-		final HttpPut httpPutValues = new HttpPut(TAVERNA_REST_API_URL + "/runs/" + workflowUUID + "/input/input/ID");
+		// Step 2
+		final File inputFile = new File(INPUT_TEST_FILE);
+		assertTrue(inputFile.exists());
+
+		reqEntity = new StringEntity(getFileOnString(INPUT_TEST_FILE));
+		reqEntity.setContentType("application/xml");
+
+		HttpPut httpPutValues = new HttpPut(TAVERNA_REST_API_URL + "/runs/" + workflowUUID + "/input/input/input");
 		httpPutValues.setEntity(reqEntity);
 		httpPutValues.setHeader("ContentType", "application/xml");
 
 		response = httpClient.execute(httpPutValues);
 		assertEquals(200, response.getStatusLine().getStatusCode());
-
+		
 		// Step 3
 		final HttpPut httpPutRun = new HttpPut(TAVERNA_REST_API_URL + "/runs/" + workflowUUID + "/status");
 		httpPutRun.setEntity(new StringEntity("Operating"));
@@ -127,18 +182,8 @@ public class TavernaClientExampleTest {
 		// Step 5
 		final HttpGet httpGetOutputDescription = new HttpGet(TAVERNA_REST_API_URL + "/runs/" + workflowUUID + "/wd/out");
 		httpGetOutputDescription.setHeader("Accept", "application/xml");
-		final String[] outputDirectoryParts = httpClient.execute(httpGetOutputDescription, new BasicResponseHandler()).split("/");
-		for (int i = 0; i < outputDirectoryParts.length; i++) {
-			if (outputDirectoryParts[i].equals("out")) {
-				final HttpGet httpGetOutput = new HttpGet(TAVERNA_REST_API_URL + "/runs/" + workflowUUID + "/wd/out/"
-						+ outputDirectoryParts[i + 1].split("\"")[0]);
-				httpGetOutput.setHeader("Accept", "text/plain");
-				System.out.println(httpClient.execute(httpGetOutput, new BasicResponseHandler()));
-				return;
-			}
-		}
-
-		fail();
+		final String outputDirectoryParts = httpClient.execute(httpGetOutputDescription, new BasicResponseHandler());
+		assertTrue(!outputDirectoryParts.isEmpty());
 	}
-
+	
 }
